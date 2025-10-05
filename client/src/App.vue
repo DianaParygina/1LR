bcae01789979448028c4208b4f7260ed8e0dd303
Diana <89041508784d@gmail.com>
2025-10-05 13:25:34 +0800

    app_before_jf

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ const router = useRouter();
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link class="nav-link" to="/login"> zxcs,m,mjnm accsВход </router-link>
+        <router-link class="nav-link" to="/login"> accsВход </router-link>
       </li>
     </ul>
   </div>
