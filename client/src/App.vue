51ce2d4459ee45fddf84432d26c11f58f0583390
Diana <89041508784d@gmail.com>
2025-10-05 12:46:51 +0800

    app

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ const router = useRouter();
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link class="nav-link" to="/login"> zxcВход </router-link>
+        <router-link class="nav-link" to="/login"> zxcsaccsВход </router-link>
       </li>
     </ul>
   </div>
