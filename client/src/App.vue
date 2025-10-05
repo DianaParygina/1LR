314160e928bc95cde116cd20448e57033b3b38b5
Diana <89041508784d@gmail.com>
2025-10-05 21:56:12 +0800

    com

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ const router = useRouter();
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link class="nav-link" to="/login"> Вхxsоvghд </router-link>
+        <router-link class="nav-link" to="/login"> Вхnxsоvghд </router-link>
       </li>
     </ul>
   </div>
