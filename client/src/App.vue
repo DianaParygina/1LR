4752716e7837d260b819e63fc3c8dc15ec37cd42
Diana <89041508784d@gmail.com>
2025-10-06 01:29:35 +0800

    commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ const router = useRouter();
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link class="nav-link" to="/login"> Вхоjkbjд </router-link>
+        <router-link class="nav-link" to="/login"> Вхоjkbnjд </router-link>
       </li>
     </ul>
   </div>
