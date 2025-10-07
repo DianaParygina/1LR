50fce740942ba4cf904c46dfbffdb1a41081ab56
Diana <89041508784d@gmail.com>
2025-10-07 12:36:42 +0800

    dtcfybuhinj

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ const router = useRouter();
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link class="nav-link" to="/login"> Вrrrrrrход </router-link>
+        <router-link class="nav-link" to="/login"> Dfygvunjokl </router-link>
       </li>
     </ul>
   </div>
